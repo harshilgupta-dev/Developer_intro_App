ad869bbfac19b32f9a2b1b99bb25a747e646ea44
I_am_developer <harshilgupta0099@gmail.com>
2021-04-29 22:51:14 +0530

    add stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Center(
             child: Text('Developer'),
           ),
-          backgroundColor: Colors.blueGrey[900],
+          backgroundColor: Colors.blueGrey,
         ),
         backgroundColor: Colors.blueGrey[200],
         body: Center(child: Image(image: NetworkImage('https://media-exp1.licdn.com/dms/image/C5603AQHqOXC5yi4kAQ/profile-displayphoto-shrink_800_800/0/1616439628698?e=1625097600&v=beta&t=9z9NrZHRWRulbkDx_XyZXC78NjfUI2gIKB_hmYSNhrI'),)),
       ),
-    ),
-  );
+    );
+  }
 }
+
