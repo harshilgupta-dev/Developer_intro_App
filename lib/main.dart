0e040ede0ab7cbaaeddd8eb245294d96cff7c344
I_am_developer <harshilgupta0099@gmail.com>
2021-05-02 14:47:41 +0530

    add Text--> FLUTTER DEVELOPER

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,22 @@ class MyApp extends StatelessWidget {
                 backgroundImage: NetworkImage(
                   'https://media-exp1.licdn.com/dms/image/C5603AQHqOXC5yi4kAQ/profile-displayphoto-shrink_800_800/0/1616439628698?e=1625702400&v=beta&t=k7KkZ_XUyLRICKV5HMC6Qt0XyDIBt_3gp8lPQGG0JqQ'
                 ),
-                radius: 50.0,
+                radius: 60.0,
               ),
               Text('Harshil Gupta',
                 style: TextStyle(
                   fontSize: 40.0,
                   fontFamily: 'Pacifico',
                   color: Colors.white
+                ),
+              ),
+              Text('FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal[900],
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  fontFamily: 'SourceSansPro',
+                  fontWeight: FontWeight.bold
                 ),
               )
 
