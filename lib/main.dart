fd975a346c8be39b4f2467b743c117f6c70b18bb
I_am_developer <harshilgupta0099@gmail.com>
2021-05-03 09:13:23 +0530

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
                   'https://media-exp1.licdn.com/dms/image/C5603AQHqOXC5yi4kAQ/profile-displayphoto-shrink_800_800/0/1616439628698?e=1625702400&v=beta&t=k7KkZ_XUyLRICKV5HMC6Qt0XyDIBt_3gp8lPQGG0JqQ'
                 ),
                 radius: 60.0,
+                backgroundColor: Colors.white,
               ),
               Text('Harshil Gupta',
                 style: TextStyle(
